70d4bf680d27164c090c8e218cac0ca0856310fd
a984983 <57126508+a984983@users.noreply.github.com>
2019-12-20 17:32:40 +0800

    12/20
    *聊天資訊上傳到Database

--- app/src/main/java/com/example/firebasechat/MessageActivity.java
+++ app/src/main/java/com/example/firebasechat/MessageActivity.java
@@ -1,13 +1,19 @@
 package com.example.firebasechat;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.firebasechat.Model.User;
@@ -19,27 +25,36 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MessageActivity extends AppCompatActivity {
 
     CircleImageView profile_image;
     TextView username;
+    ImageButton btn_send;
+    EditText text_send;
 
     FirebaseUser firebaseUser;
     DatabaseReference reference;
 
     Intent intent;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
 
+        //獲取狀態欄改變背景顏色 API Level21(5.0)以下可能會報錯
+        Window window = this.getWindow();
+        window.setStatusBarColor(getResources().getColor(R.color.colorOrange500));
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//設置返回鍵
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,11 +64,26 @@ public class MessageActivity extends AppCompatActivity {
 
         profile_image = findViewById(R.id.profile_image);
         username = findViewById(R.id.username);
+        btn_send = findViewById(R.id.btn_send);
+        text_send = findViewById(R.id.text_send);
 
         intent = getIntent();
-        String userID = intent.getStringExtra("userID");
-
+        final String userID = intent.getStringExtra("userID");
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = text_send.getText().toString();
+                if(!msg.equals("")){
+                    sendMessage(firebaseUser.getUid(),userID,msg);
+                }else {
+                    Toast.makeText(MessageActivity.this , "請輸入訊息" , Toast.LENGTH_SHORT).show();
+                }
+                text_send.setText("");
+            }
+        });
+
         reference = FirebaseDatabase.getInstance().getReference("Users").child(userID);//指定的使用者
 
         reference.addValueEventListener(new ValueEventListener() {
@@ -73,5 +103,17 @@ public class MessageActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void sendMessage(String sender , String receiver , String Message){
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+        HashMap<String , Object> hashMap = new HashMap<>();
+        hashMap.put("sender" , sender);
+        hashMap.put("receiver" , receiver);
+        hashMap.put("Message" , Message);
+
+        reference.child("Chats").push().setValue(hashMap);
     }
 }
